b5ffd42003140f76c9b0d963b7e449800e8d8759
陳虹如 <victoria1015@hotmail.com>
2017-10-30 23:18:05 +0800

    Initial Commit

---
+++ PERLER BEADS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PERLER BEADS
+//
+//  Created by Victoria on 2017/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
